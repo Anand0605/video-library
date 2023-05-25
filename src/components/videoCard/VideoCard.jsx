670cd38fc3b19c6554fc2cf9b72c3632d56aef6c
Anand0605 <anandvns0510@gmail.com>
2023-05-25 11:46:05 +0530

    update

--- src/components/videoCard/VideoCard.jsx
+++ src/components/videoCard/VideoCard.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react'
 import { AiFillPlayCircle } from 'react-icons/ai'
 import './VideoCard.css'
-// import play from '../../images/play-button.png'
+import play from '../../images/play-button.png'
 
 const VideoCard = ({ video }) => {
     const [image, setImage] = useState(video.image)
 
     return (
         <div className="video-card">
-            <div className="thumbnail" onMouseOver={() => setImage(<AiFillPlayCircle />)} onMouseOut={() => setImage(video.image)} >
+            <div className="thumbnail" onMouseOver={() => setImage(play)} onMouseOut={() => setImage(video.image)} >
                 {video.image && <img src={image} alt="" />}
             </div>
             <div className="video-description">
