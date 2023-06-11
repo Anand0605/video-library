a82cb34aca5116b21fb535197df3a7e8df4d3e98
Anand0605 <anandvns0510@gmail.com>
2023-06-11 23:13:21 +0530

    update

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,14 +6,31 @@ const Home = () => {
     return (
         <>
             <div className="main-gallery">
-                <div className="gallery1"></div>
-                <div className="gallery2">
-                    <div className="go-to-video"><Link to='/gallary'><button>Go To Video Gallery</button></Link></div>
-                </div>
-                <div className="gallery3">
-                    <br /><br /><hr />
+                <div className="main-page"
+                >   <div className="go-to-video"><Link to='/gallary'><button>Go To Video Gallery</button></Link>
+                    </div>
                 </div>
             </div>
+            <br />
+            <br />
+            <hr />
+            <br />
+            <h2>Indian Cricket player Details (Male & Female)</h2>
+            <br />
+            <div className="player-deatils">
+                <div className="gallery1"><button className='btn' type="button">M S Dhoni</button></div>
+                <div className="gallery2"><button className='btn' type="button">Virat Kohli</button></div>
+                <div className="gallery3"><button className='btn' type="button">Kl Rahul</button></div>
+                <div className="gallery4"><button className='btn' type="button">Rohit Sharma</button></div>
+                <div className="gallery5"><button className='btn' type="button">Mithali Raj</button></div>
+                <div className="gallery6"><button className='btn' type="button">Smriti Mandhana</button></div>
+                <div className="gallery7"><button className='btn' type="button">Harman Preet Kaur</button></div>
+                <div className="gallery8"><button className='btn' type="button">Harleen Deol</button></div>
+
+
+            </div>
+
+
         </>
     )
 }
