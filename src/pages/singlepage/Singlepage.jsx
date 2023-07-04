72261f9cea90627022ea412e8adf1d26ec7f75b4
Anand0605 <anandvns0510@gmail.com>
2023-07-04 21:46:19 +0530

    update

--- src/pages/singlepage/Singlepage.jsx
+++ src/pages/singlepage/Singlepage.jsx
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import './singlepage.css'
-import { Link, useParams } from 'react-router-dom';
-// import msd from '../../images/MS-Dhoni.jpg'
+import { NavLink, useParams } from 'react-router-dom';
+import loading from '../../images/1487.gif'
 
 const Singlepage = () => {
     const [videoData, setVideoData] = useState()
+    const [isLoad, setIsLoad] = useState(false)
 
     const { id } = useParams()
-    console.log(id)
+
     const getVideo = async () => {
+        setIsLoad(true)
         try {
             const res = await fetch(`/api/video/${id}`)
             const data = await res.json()
@@ -17,47 +19,59 @@ const Singlepage = () => {
             console.log(err)
 
         }
+        finally {
+            setIsLoad(false)
+        }
     }
     useEffect(() => {
         getVideo()
     }, [])
-    console.log(videoData)
+    // console.log(videoData)
+    // console.log(videoData?.videoLink)
 
 
     return (
         <>
-            <div className="mainsinglepage">
-                <div className="left-singlepage">
-                    <div className="left-singlepage-video">
-                        <Link to={videoData?.videoLink}></Link>
-                        <img src={videoData?.image} alt="" />
-                        <h1> {videoData?.title}</h1>
-                        <p>{videoData?.creator}</p>
+            {
+                isLoad ?
+                    <div className="loading">
+                        <img src={loading} alt="" />
+                    </div> :
+                    <div className="mainsinglepage">
+                        <div className="left-singlepage">
+                            <div className="left-singlepage-video">
+                                <iframe width="560" height="315" src={`https://www.youtube.com/embed/${videoData?.videoId}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                                <h1> {videoData?.title}</h1>
+                                <p>{videoData?.creator}</p>
+
+                                <div className="like-btn">
+                                    <button className='btn1'>like</button>
+                                    <button className='btn1'>share</button>
+                                    <button className='btn1'>save</button>
+                                </div>
 
 
-                        <div className="iframe">
-                            {/* <iframe width="560" height="215" src={`https://www.youtube.com/embed/${videoData._id}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
+                                {/* <div className="iframe">
+                            <iframe width="560" height="215" src={`https://www.youtube.com/embed/${videoData._id}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                        </div> */}
+
+                            </div>
+                            <div className="left-singlepage-comments">
+
+
+                            </div>
                         </div>
-                        <div className="like-btn">
-                            {/* <button className='btn1'>like</button>
-                            <button className='btn1'>share</button>
-                            <button className='btn1'>save</button> */}
+                        <div className="right-singlepage">
+                            <h2>Suggest Video</h2>
+                            <div className="suggest-video">
+
+                            </div>
+                            <div className="suggest-discription">
+
+                            </div>
                         </div>
                     </div>
-                    <div className="left-singlepage-comments">
-
-                    </div>
-                </div>
-                <div className="right-singlepage">
-                    <h2>Suggest Video</h2>
-                    <div className="suggest-video">
-
-                    </div>
-                    <div className="suggest-discription">
-
-                    </div>
-                </div>
-            </div>
+            }
 
         </>
 
